eeb5e92307ea271c2e41ec641ea966c907823518
w-xlter <palmieriwalter.business@gmail.com>
2026-04-02 16:58:47 +0200

    styled the page minimally

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import * as UI from "./ui.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("btn").addEventListener("click", () => {
         UI.print()
-    })
 })
